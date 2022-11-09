be37953526e72c5a06f01ea19c4e90a98d11aea2
ElenaBoyko1992 <Bionka@inbox.ru>
2022-11-09 15:00:46 +0300

    09.11.2022

---
+++ src/Main/Main.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import style from './Main.module.css'
+
+
+function Main() {
+    return (
+        <div className={style.mainBlock}>
+            <div className={style.container}>
+                <div className={style.text}>
+                    <span>Hi!</span>
+                    <h1>My name is Elena Boiko.</h1>
+                    <p>I`m a frontend developer.</p>
+                </div>
+                <div className={style.photo}></div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Main;
