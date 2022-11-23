525846728576f0ca761cebf766e5ed822f7b0381
ElenaBoyko1992 <Bionka@inbox.ru>
2022-11-23 22:06:10 +0300

    23.11.2022

--- src/Skills/Skills.tsx
+++ src/Skills/Skills.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './Skills.module.css'
 import styleContainer from '../common/Styles/Container.module.css'
+import Skill from "./Skill/Skill";
 
 function Skills() { //ПОТОМ ПРОМАПИТЬ!
     return (
@@ -8,7 +9,13 @@ function Skills() { //ПОТОМ ПРОМАПИТЬ!
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <h2 className={style.title}>Skills</h2>
                 <div className={style.skills}>
+                    <Skill title={'JS'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, adipisci ducimus eaque illo ipsam laudantium molestiae nesciunt optio possimus provident.'}/>
+                    <Skill title={'CSS'}
+                           description={'Quos sequi soluta tempore velit, voluptatibus? Provident saepe sapiente ullam.'}/>
 
+                    <Skill title={'React'}
+                           description={'Adipisicing elit. Accusamus, adipisci ducimus eaque illo ipsam laudantium molestiae nesciunt optio possimus provident.'}/>
                 </div>
             </div>
         </div>
