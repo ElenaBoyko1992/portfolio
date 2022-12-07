df6e08bc14568f9482ca3276a982f44aab364e65
ElenaBoyko1992 <Bionka@inbox.ru>
2022-12-07 22:38:11 +0300

    07.12.2022

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -6,7 +6,7 @@ import styleContainer from '../common/Styles/Container.module.css'
 function Main() {
     return (
         <div className={style.mainBlock}>
-            <div className={styleContainer.container}>
+            <div className={`${styleContainer.container} ${style.mainContainer}`}>
                 <div className={style.text}>
                     <span>Hi!</span>
                     <h1>My name is Elena Boiko.</h1>
