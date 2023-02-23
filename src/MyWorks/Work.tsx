e40d04d9051528bd24181998a1d6c698a7475921
ElenaBoyko1992 <Bionka@inbox.ru>
2023-02-23 15:28:15 +0300

    23.02.2023

--- src/MyWorks/Work.tsx
+++ src/MyWorks/Work.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './Work.module.scss'
 
 type WorkPropsType = {
@@ -9,12 +9,21 @@ type WorkPropsType = {
 }
 
 function Work(props: WorkPropsType) {
+    let [buttonIsHover, setButtonIsHover] = useState(false)
+    const onMouseEnterHandler = () => {
+        setButtonIsHover(true)
+    }
+    const onMouseLeaveHandler = () => {
+        setButtonIsHover(false)
+    }
 
     return (
         <div className={style.work}>
-            <div className={style.preview}
-                 style={{backgroundImage: `url('${props.img}')`}}>
-                <a className={style.button} href={props.link}>Watch</a>
+            <div
+                className={buttonIsHover ? `${style.preview} ${style.hoveredButton}` : style.preview}
+                style={{backgroundImage: `url('${props.img}')`}}>
+                <a className={style.button} href={props.link} onMouseEnter={onMouseEnterHandler}
+                   onMouseLeave={onMouseLeaveHandler}>Watch</a>
             </div>
             <div className={style.description}>
                 <div className={style.projectName}>
