3d742b97a68b9b98fb8f688abd8dc56a71a057b6
ElenaBoyko1992 <Bionka@inbox.ru>
2023-04-14 14:54:16 +0300

    14.04.2023 - ts particles library added

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -2,11 +2,111 @@ import React from 'react';
 import style from './Main.module.scss'
 import styleContainer from '../common/Styles/CommonStyles.module.scss'
 import photo from '../assets/images/myPhoto.jpeg'
+import {useCallback} from "react";
+import Particles from "react-tsparticles";
+import type {Container, Engine} from "tsparticles-engine";
+import {loadFull} from "tsparticles";
+
+
 
 
 function Main() {
+    const particlesInit = useCallback(async (engine: Engine) => {
+        console.log(engine);
+
+        // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container: Container | undefined) => {
+        await console.log(container);
+    }, []);
+
     return (
+
         <div id={'main'} className={style.mainBlock}>
+
+            <Particles className={style.tsparticles}
+                       init={particlesInit}
+                       loaded={particlesLoaded}
+
+                       options={{
+
+                           fullScreen: false,
+                           background: {
+                               color: {
+                                   value: "transparent",
+                               },
+                           },
+                           fpsLimit: 120,
+                           interactivity: {
+                               events: {
+                                   onClick: {
+                                       enable: false,
+                                       mode: "push",
+                                   },
+                                   onHover: {
+                                       enable: false,
+                                       mode: "repulse",
+                                   },
+                                   resize: true,
+                               },
+                               modes: {
+                                   push: {
+                                       quantity: 4,
+                                   },
+                                   repulse: {
+                                       distance: 200,
+                                       duration: 0.4,
+                                   },
+                               },
+                           },
+                           particles: {
+                               color: {
+                                   value: "#ffffff",
+                               },
+                               links: {
+                                   color: "#ffffff",
+                                   distance: 150,
+                                   enable: true,
+                                   opacity: 0.5,
+                                   width: 1,
+                               },
+                               collisions: {
+                                   enable: true,
+                               },
+                               move: {
+                                   direction: "none",
+                                   enable: true,
+                                   outModes: {
+                                       default: "bounce",
+                                   },
+                                   random: false,
+                                   speed: 3,
+                                   straight: false,
+                               },
+                               number: {
+                                   density: {
+                                       enable: true,
+                                       area: 800,
+                                   },
+                                   value: 80,
+                               },
+                               opacity: {
+                                   value: 0.5,
+                               },
+                               shape: {
+                                   type: "circle",
+                               },
+                               size: {
+                                   value: {min: 1, max: 5},
+                               },
+                           },
+                           detectRetina: true,
+                       }}
+            />
             <div className={`${styleContainer.container} ${style.mainContainer}`}>
                 <div className={style.photo}>
                     <img src={photo}/>
@@ -20,7 +120,8 @@ function Main() {
             </div>
 
         </div>
-    );
+    )
+
 }
 
 export default Main;
